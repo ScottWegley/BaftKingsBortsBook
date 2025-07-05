1c7afabf80f38fc262105dc46c5b757ae9597d1c
Scott Wegley <scbrwegley@protonmail.ch>
2025-07-05 10:14:59 -0700

    cleared canon directory, prepping to run on private infra

---
+++ daily_event.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git pull
+pip3 install -r requirements.txt
+
+python3 src/main.py --output --canon --headless --rng-mode date
+python3 send_first_output_to_discord.py
+
+git add .
+git commit -m "Ran event for $DATE" || echo "No changes to commit"
+git push || echo "No changes to push"
